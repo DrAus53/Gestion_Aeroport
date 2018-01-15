f4b6a832a8bc5efde1bc888d6cffb37e3ac65439
Tanguy SUARD <suard.tanguy@gmail.com>
2018-01-15 08:54:46 +0100

    creation DAOAeroport et mise en commentaire pb modele aeroport

--- src/fr/eseo/gestionAeroport/dao/DAO.java
+++ src/fr/eseo/gestionAeroport/dao/DAO.java
@@ -1,5 +1,43 @@
 package fr.eseo.gestionAeroport.dao;
 
-public abstract class DAO<T> {
+import fr.eseo.gestionAeroport.controleur.baseDeDonnees.ConnexionBDD;
 
+public abstract class DAO<T> {
+	protected ConnexionBDD connect = null;
+
+	public DAO(ConnexionBDD connect) {
+		this.connect = connect;
+	}
+
+	/**
+	 * Méthode de création
+	 * 
+	 * @param obj
+	 * @return boolean
+	 */
+	public abstract boolean create(T obj);
+
+	/**
+	 * Méthode pour effacer
+	 * 
+	 * @param obj
+	 * @return boolean
+	 */
+	public abstract boolean delete(T obj);
+
+	/**
+	 * Méthode de mise à jour
+	 * 
+	 * @param obj
+	 * @return boolean
+	 */
+	public abstract boolean update(T obj);
+
+	/**
+	 * Méthode de recherche des informations
+	 * 
+	 * @param id
+	 * @return T
+	 */
+	public abstract T find(int id);
 }
--- src/fr/eseo/gestionAeroport/modele/Aeroport.java
+++ src/fr/eseo/gestionAeroport/modele/Aeroport.java
@@ -1,64 +1,71 @@
 package fr.eseo.gestionAeroport.modele;
 
 public class Aeroport {
-	
-	
-public int getIdAeroport() {
+
+	public int getIdAeroport() {
 		return idAeroport;
 	}
-public void setIdAeroport(int idAeroport) {
+
+	public void setIdAeroport(int idAeroport) {
 		this.idAeroport = idAeroport;
 	}
-public String getNom() {
+
+	public String getNom() {
 		return nom;
 	}
-public void setNom(String nom) {
+
+	public void setNom(String nom) {
 		this.nom = nom;
 	}
-public String getVille() {
+
+	public String getVille() {
 		return ville;
 	}
-public void setVille(String ville) {
+
+	public void setVille(String ville) {
 		this.ville = ville;
 	}
-public String getPays() {
+
+	public String getPays() {
 		return pays;
 	}
-public void setPays(String pays) {
+
+	public void setPays(String pays) {
 		this.pays = pays;
 	}
-public String getNomTerminal() {
+
+	public String getNomTerminal() {
 		return nomTerminal;
 	}
-public void setNomTerminal(String nomTerminal) {
+
+	public void setNomTerminal(String nomTerminal) {
 		this.nomTerminal = nomTerminal;
 	}
 
-public int idAeroport;
-public String nom;
-public String ville;
-public String pays;
-public String nomTerminal;
+	public int idAeroport;
+	public String nom;
+	public String ville;
+	public String pays;
+	public String nomTerminal;
 
-public Aeroport(int idAeroport,String nom,String nomTerminal,String ville,String pays) {
-	this.idAeroport=idAeroport;
-	this.nom=nom;
-	this.ville=ville;
-	this.pays=pays;
-	this.nomTerminal=nomTerminal;
-}
-public void ajouter() {
-	basededonnees.ajouter(new Aeroport(idAeroport,nom,ville,pays,nomTerminal));
-}
+	public Aeroport(int idAeroport, String nom, String nomTerminal, String ville, String pays) {
+		this.idAeroport = idAeroport;
+		this.nom = nom;
+		this.ville = ville;
+		this.pays = pays;
+		this.nomTerminal = nomTerminal;
+	}
 
-public void supprimer(Aeroport aeroport) {
-	basededonnees.supprimer(aeroport);
-}
+	public void ajouter() {
+		// basededonnees.ajouter(new Aeroport(idAeroport,nom,ville,pays,nomTerminal));
+	}
 
-public void modifier() {
-	basededonnees.modifier(new Aeroport(idAeroport,nom,ville,pays,nomTerminal));
-}
+	public void supprimer(Aeroport aeroport) {
+		// basededonnees.supprimer(aeroport);
+	}
 
-
+	public void modifier() {
+		// basededonnees.modifier(new Aeroport(idAeroport,nom,ville,pays,nomTerminal));
+	}
 
 }
