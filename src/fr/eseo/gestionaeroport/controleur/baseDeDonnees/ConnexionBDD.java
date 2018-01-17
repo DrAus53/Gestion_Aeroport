d6e2379cfaff0e408135d5545a2e37cda0873712
Damien Maymard <Damien Maymard@172.21.1.67>
2018-01-17 09:16:58 +0100

    Suppression des test de connexion et finalisation de la classe de connexion pour utilisation dans les DAO

--- src/fr/eseo/gestionaeroport/controleur/baseDeDonnees/ConnexionBDD.java
+++ src/fr/eseo/gestionaeroport/controleur/baseDeDonnees/ConnexionBDD.java
@@ -4,21 +4,39 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class ConnexionBDD {
-	public static void main(String[] args) {
 
+	public static void main(String[] args) {
 		try {
 			Class.forName("org.postgresql.Driver");
-			System.out.println("Driver O.K.");
-
-			String url = "jdbc:postgresql://192.168.4.127/phppgadmin";
-			String user = "java";
-			String passwd = "javaConnectBD986uhGt523because";
-
+			String url = "jdbc:postgresql://192.168.4.127:5432/gestionaeroport";
+			String user = "";
+			String passwd = "";
 			Connection connect = DriverManager.getConnection(url, user, passwd);
-			System.out.println("Connexion effective !");
-
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+
 }
+
+/*
+ * // Création d'un objet Statement Statement state = conn.createStatement(); //
+ * L'objet ResultSet contient le résultat de la requête SQL ResultSet result =
+ * state.executeQuery("SELECT * FROM vol"); // On récupère les MetaData
+ * ResultSetMetaData resultMeta = result.getMetaData();
+ * 
+ * System.out.println("\n**********************************"); // On affiche le
+ * nom des colonnes for (int i = 1; i <= resultMeta.getColumnCount(); i++)
+ * System.out.print("\t" + resultMeta.getColumnName(i).toUpperCase() + "\t *");
+ * 
+ * System.out.println("\n**********************************");
+ * 
+ * while (result.next()) { for (int i = 1; i <= resultMeta.getColumnCount();
+ * i++) System.out.print("\t" + result.getObject(i).toString() + "\t |");
+ * 
+ * System.out.println("\n---------------------------------");
+ * 
+ * }
+ * 
+ * result.close(); state.close();
+ */
