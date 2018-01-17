5be28a5c29596c027a0cb488b0a0e82ce3afa5e9
Tanguy SUARD <suard.tanguy@gmail.com>
2018-01-17 10:04:52 +0100

    changement panneau embarquement plsu beau

--- src/fr/eseo/gestionaeroport/vue/panneaux/PanneauEmbarquement.java
+++ src/fr/eseo/gestionaeroport/vue/panneaux/PanneauEmbarquement.java
@@ -1,8 +1,11 @@
 package fr.eseo.gestionaeroport.vue.panneaux;
 
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -12,10 +15,8 @@ import javax.swing.JTextField;
 
 import fr.eseo.gestionaeroport.vue.ui.FenetreGestionAeroport;
 
+@SuppressWarnings("serial")
 public class PanneauEmbarquement extends JPanel {
-
-	private static final long serialVersionUID = 1L;
-	private FenetreGestionAeroport fenetreGestionAeroport;
 
 	public PanneauEmbarquement(int largeur, int hauteur) {
 		this.setSize(largeur, hauteur);
@@ -30,32 +31,55 @@ public class PanneauEmbarquement extends JPanel {
 
 	private void initComponent() {
 		this.setLayout(new GridBagLayout());
-
-		this.setBackground(Color.PINK);
-		// this.setSize(50, 150);
-		// this.setBounds(0, 0, 50, 150);
 		GridBagConstraints constraints = new GridBagConstraints();
-		// constraints.gridwidth = 1;
+		constraints.insets = new Insets(2, 2, 2, 2);
 
-		JLabel labelEmbarquement = new JLabel("Embarquement");
+		JLabel zoneTitre = new JLabel("Embarquement");
+		zoneTitre.setFont(new Font("Berlin Sans FB Demi", 1, 30));
+		zoneTitre.setForeground(FenetreGestionAeroport.COULEUR_CADRE_PANNEAUX);
 		constraints.gridx = 0;
 		constraints.gridy = 0;
-		this.add(labelEmbarquement, constraints);
+		constraints.gridheight = 1;
+		constraints.gridwidth = 3;
+		this.add(zoneTitre, constraints);
 
-		JLabel labelNumVol = new JLabel("Numéro de vol : ");
+		JLabel labelNumVol = new JLabel("Numéro de vol  ");
 		constraints.gridx = 0;
 		constraints.gridy = 1;
+		constraints.gridheight = 1;
+		constraints.gridwidth = 1;
 		this.add(labelNumVol, constraints);
 
 		JTextField jTextFieldNumVol = new JTextField();
+		jTextFieldNumVol.setColumns(15);
+		jTextFieldNumVol
+				.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, FenetreGestionAeroport.COULEUR_ACCENTUATION));
 		constraints.gridx = 1;
 		constraints.gridy = 1;
+		constraints.gridheight = 1;
+		constraints.gridwidth = 1;
 		this.add(jTextFieldNumVol, constraints);
 
+		// valider
 		JButton jButtonValider = new JButton("Valider");
+		jButtonValider.setPreferredSize(new Dimension(70, 30));
+		/// couleur du texte
+		jButtonValider.setForeground(Color.WHITE);
+		/// on peint l'interieur
+		jButtonValider.setBackground(FenetreGestionAeroport.COULEUR_ACCENTUATION);
+		/// on peint les bordures
+		jButtonValider
+				.setBorder(BorderFactory.createMatteBorder(2, 2, 2, 2, FenetreGestionAeroport.COULEUR_ACCENTUATION));
+		/// on enleve l'effet focus
+		jButtonValider.setFocusPainted(false);
+		/// on positionne la case
 		constraints.gridx = 2;
-		constraints.gridy = 0;
-		constraints.gridheight = 2;
+		constraints.gridy = 1;
+		/// on choisit la taille
+		constraints.gridheight = GridBagConstraints.REMAINDER;
+		constraints.gridwidth = GridBagConstraints.REMAINDER;
+		/// remplissage de la case en horizontal et vertical
+		constraints.fill = GridBagConstraints.BOTH;
 		this.add(jButtonValider, constraints);
 	}
 }
